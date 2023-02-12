54dd0e149f42ff87c59028af4a7e918e77f2db05
jjohnson673 <jkjohnson673@gmail.com>
2023-02-12 11:24:36 -0500

    folder rearranging

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <About />
+        <Portfolio />
+        <Contact />
+        <Footer />
+    </>
+  )
+}
+
+export default App
